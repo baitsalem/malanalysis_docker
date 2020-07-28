8b8a5b11611b46b43121f8effc099f9f4f8e747a
baitsalem <baitsalem@gmail.com>
2020-07-28 08:44:09 +0200

    entry point in progress

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,13 @@ if [ ! -e /dev/kvm ]; then
 fi
 
 cuckoo --cwd /opt/cuckoo
+cp -r conf /opt/cuckoo/.cuckoo
 
+virt-install --name=cuckoo --vcpus=1 --memory=1024 --disk /VMS/win12.qcow --import
+
+snapshot_name=$(virsh snapshot-create cuckoo | cut -d ' ' -f 3)
+
+if [ ! -z ${snapshot_name} ]
+
+fi
 
